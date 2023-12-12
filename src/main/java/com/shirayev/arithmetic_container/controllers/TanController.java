2a90e1fa73b2577d2eb41722ffd6a97a87338033
Shirayev Aleksey <aleksi.aleksi2014@yandex.ru>
2023-12-12 20:51:34 +0300

    рефакторинг контроллера

--- src/main/java/com/shirayev/arithmetic_container/controllers/TanController.java
+++ src/main/java/com/shirayev/arithmetic_container/controllers/TanController.java
@@ -1,25 +1,25 @@
 package com.shirayev.arithmetic_container.controllers;
 
-import com.shirayev.arithmetic_container.dto.EMeasurementType;
 import com.shirayev.arithmetic_container.dto.Request;
 import com.shirayev.arithmetic_container.dto.Response;
+import com.shirayev.arithmetic_container.servisies.TanService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/pmi-120")
 public class TanController {
+
+    private final TanService tanService;
 
     @PostMapping("/tan")
     public ResponseEntity<Response> handlerGetTanResult(@RequestBody Request request) {
 
-        EMeasurementType type = EMeasurementType.valueOf(request.getMeasurementType());
-
-        Double result = type.equals(EMeasurementType.RADIAN) ?
-                Math.tan(request.getValue()) : Math.tan(request.getValue() * (Math.PI / 180));
-
+        Double result = tanService.tan(request);
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Response.builder()
